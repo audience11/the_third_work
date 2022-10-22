150bead7543fb166a4856f409a5935c94ef0d9f8
audience11 <112678017+audience11@users.noreply.github.com>
2022-10-22 09:22:27 +0800

    Add files via upload

---
+++ 第三次Java实验/two/Student.java
@@ -0,0 +1,11 @@
+package two;
+
+public class Student extends Teacher{
+    protected int num =1;
+    protected String courses[];
+    public Student(String name, int id, String courses) {
+        super();
+        this.name = name;
+        this.id = id;
+}
+}
